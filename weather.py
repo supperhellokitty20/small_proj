86858d64510c81bb656ba12bdb005be730f979c3
Tuan Nguyen <supperhellokitty20@gmail.com>
2020-01-25 22:40:52 -0500

    initial

---
+++ weather.py
@@ -0,0 +1,25 @@
+import ipapi
+import requests
+import json 
+#get the location 
+ipapi.location(ip=None, key=None, field=None)
+a= ipapi.location(None,None,'city')
+key = '75479d19a2eca944f49dfbf0cdaadd8b'
+#get current weather and load data
+response = requests.get('http://api.openweathermap.org/data/2.5/weather?q={a}&APPID={key}'.format(a=a,key=key))
+data = response.json()['main']
+#sort jsonfile 
+feels_like =str(data['feels_like'])
+humid= str(data['humidity'])
+cur_temp =str(data['temp'])
+temp_max=str(data['temp_max'])
+temp_min= str(data['temp_min'])
+
+print(feels_like,humid,cur_temp,temp_max,temp_min) 
+
+'''def format(obj):
+    text = json.dumps(obj,sort_keys=True, indent=4) 
+    print text['feels_like']'''
+
+#Print out result 
+
